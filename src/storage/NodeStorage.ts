945cebdec8f0d5422bccd63553e9de83f7fd1e14
Mathias Bouhon Keulen <mathias.bouhonkeulen@student.kuleuven.be>
2026-03-11 23:33:22 +0100

    implemented new interfaces

---
+++ src/storage/NodeStorage.ts
@@ -0,0 +1,11 @@
+import { MetaStorage } from "./MetaStorage";
+import { ConfigStorage } from "./ConfigStorage";
+import { LogStorage } from "./LogStorage";
+import { SnapshotStorage } from "./SnapshotStorage";
+
+export interface NodeStorage {
+    meta: MetaStorage;
+    config: ConfigStorage;
+    log: LogStorage;
+    snapshot: SnapshotStorage;
+}
